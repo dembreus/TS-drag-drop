45f2fa972d26f1812ee0e565c9a56eb894c3b7ee
Dillon <dillon.embreus@gmail.com>
2019-12-08 23:42:20 -0700

    Modularized code and updated project structure

---
+++ src/components/base-component.ts
@@ -0,0 +1,38 @@
+// Compoent Base Class
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    inserAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+
+    this.attach(inserAtStart);
+  }
+  private attach(inserAtStart: boolean) {
+    this.hostElement.insertAdjacentElement(
+      inserAtStart ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
